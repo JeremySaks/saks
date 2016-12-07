cb29e34a5d1007175c7796eef6ae5725f675c1d2
Jeremy Saks <jeremy.saks@gmail.com>
2016-12-06 21:39:56 -0500

    :boar:

--- src/root/root.js
+++ src/root/root.js
@@ -5,31 +5,14 @@ import Contact from '../pages/contact/contact.js';
 import Projects from '../pages/projects/projects.js';
 import NotFound from '../pages/notfound/notfound.js';
 
-const {NODE_ENV} = process.env;
-
-const initializeAnalytics = () => {
-  if (NODE_ENV === 'development') return;
-  window.ga = () => {
-    (window.ga.q = window.ga.q || []).push(arguments);
-  };
-  const {ga} = window;
-  ga.l =+ new Date();
-  ga('create', 'UA-88258965-1', 'auto');
-  ga('set', 'page', window.location.pathname);
-  ga('send', 'pageview');
-}
-
-const trackAnalytics = ({pathname}) => {
-  if (NODE_ENV === 'development') return;
+const analytics = ({pathname}) => {
   const {ga} = window;
   if (!ga) return;
   ga('set', 'page', pathname);
   ga('send', 'pageview');
 }
 
-window.onload = initializeAnalytics;
-
-browserHistory.listen(trackAnalytics);
+browserHistory.listen(analytics);
 
 const Root = <Router
   history={browserHistory}>
